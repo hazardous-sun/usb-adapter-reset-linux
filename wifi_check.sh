75fd74fb668d61ad9e80d5f071c49d5a93d3e394
solaire-Pc <contact@unharmfulhazzard.com>
2023-08-20 16:20:11 -0300

    Replaced grep with nmcli device show

--- wifi_check.sh
+++ wifi_check.sh
@@ -3,19 +3,21 @@
 # Define paths
 SCRIPT_DIR=$(dirname "$0")
 USB_RESET_SCRIPT="$SCRIPT_DIR/usb_reset"
-USB_PATH="/dev/bus/usb/001/006"
+USB_PATH="/dev/bus/usb/001/003"
 LOG_FILE="$SCRIPT_DIR/reset_script.log"
 
 while true; do
-  # Run nmcli command and check for "disconnected" keyword
-  if nmcli device | grep 'wifi' -q | awk '$3 == "disconnected" { print $3 }' == "disconected"; then
-      echo "$(date): A connected device was found, but it is disconnected. Running usb_reset..." >> "$LOG_FILE"
-            "$USB_RESET_SCRIPT" "$USB_PATH"
-  # Run nmcli command and check for "unavailable" keyword
-  elif nmcli device | grep 'wifi' -q | awk '$3 == "unavailable" { print $3 }' == "unavailable"; then
-      echo "$(date): No connected device found on $USB_PATH" >> "$LOG_FILE"
+  # Run nmcli command and check for WiFi connection status
+  wifi_status=$(nmcli device show | awk -v devtype="wifi" '$2 == devtype {print $NF}')
+
+  if [ "$wifi_status" == "disconnected" ]; then
+    echo "$(date): A WiFi device was found, but it is disconnected. Running usb_reset..." >> "$LOG_FILE"
+    "$USB_RESET_SCRIPT" "$USB_PATH"
+  elif [ "$wifi_status" == "connected" ]; then
+    echo "$(date): A WiFi device was found and is connected." >> "$LOG_FILE"
   else
-      echo "$(date): A connected device was found." >> "$LOG_FILE"
+    echo "$(date): No WiFi device found on $USB_PATH" >> "$LOG_FILE"
   fi
+
   sleep 120 # Wait for 2 minutes
 done
