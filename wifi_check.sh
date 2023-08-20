9c29204e5dc724386657e4637bda62f5cce992b5
solaire-Pc <contact@unharmfulhazzard.com>
2023-08-20 12:27:24 -0300

    Refactored the script
    Added absolute ppaths, error handling and logging

--- wifi_check.sh
+++ wifi_check.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Run nmcli command and check for "disconnected" keyword
-if nmcli device | grep -q "disconnected"; then
-    echo "No connected device found. Running usb_reset..."
-    ./usb_reset "/dev/bus/usb/001/006"
-else
-    echo "A connected device was found."
-fi
+# Define paths
+SCRIPT_DIR=$(dirname "$0")
+USB_RESET_SCRIPT="$SCRIPT_DIR/usb_reset"
+USB_PATH="/dev/bus/usb/001/006"
+LOG_FILE="$SCRIPT_DIR/reset_script.log"
+
+while true; do
+  # Run nmcli command and check for "disconnected" keyword
+  if nmcli device | grep -q "disconnected"; then
+      echo "$(date): No connected device found. Running usb_reset..." >> "$LOG_FILE"
+      "$USB_RESET_SCRIPT" "$USB_PATH"
+  else
+      echo "$(date): A connected device was found." >> "$LOG_FILE"
+  fi
+  sleep 30 # Wait for 30 seconds
+done
