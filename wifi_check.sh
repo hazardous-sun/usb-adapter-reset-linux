3e0acd20444600c0d9a104a9e2b6320ad518b586
solaire-Pc <contact@unharmfulhazzard.com>
2023-08-20 17:03:07 -0300

    The USB bus and port are no longer static
    The script will determine the bus and port in each iteration.

--- wifi_check.sh
+++ wifi_check.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Define paths
-SCRIPT_DIR=$(dirname "$0")
-USB_RESET_SCRIPT="$SCRIPT_DIR/usb_reset"
-CURRENT_USB_BUS=$(lsusb | grep RTL88x2bu | awk '{print $2}')
-CURRENT_USB_PORT=$(lsusb | grep RTL88x2bu | awk '{print $4}')
-USB_PATH="/dev/bus/usb/$CURRENT_USB_BUS/$CURRENT_USB_PORT"
-LOG_FILE="$SCRIPT_DIR/reset_script.log"
-
 while true; do
+
+  # Define paths
+  SCRIPT_DIR=$(dirname "$0")
+  USB_RESET_SCRIPT="$SCRIPT_DIR/usb_reset"
+  CURRENT_USB_BUS=$(lsusb | grep RTL88x2bu | awk '{print $2}')
+  CURRENT_USB_PORT=$(lsusb | grep RTL88x2bu | awk '{print $4}')
+  USB_PATH="/dev/bus/usb/$CURRENT_USB_BUS/$CURRENT_USB_PORT"
+  LOG_FILE="$SCRIPT_DIR/reset_script.log"
+
   # Get WiFi device status
   wifi_status=$(nmcli device show wlx90de80ae8d61 | awk -F": " '/GENERAL.STATE/ {gsub(/^[ \t]+|[()0-9]+/,"",$2); sub(/^[ \t]+/, "", $2); print $2}')
 
