c59344654ccbd81b2c81547ad3aca5388eac046d
solaire-Pc <contact@unharmfulhazzard.com>
2023-08-20 16:43:57 -0300

    Fixed the nmcli device show logic to filter the connection

--- wifi_check.sh
+++ wifi_check.sh
@@ -7,17 +7,17 @@ USB_PATH="/dev/bus/usb/001/003"
 LOG_FILE="$SCRIPT_DIR/reset_script.log"
 
 while true; do
-  # Run nmcli command and check for WiFi connection status
-  wifi_status=$(nmcli device show | awk -v devtype="wifi" '$2 == devtype {print $NF}')
+  # Get WiFi device status
+  wifi_status=$(nmcli device show wlx90de80ae8d61 | awk -F": " '/GENERAL.STATE/ {gsub(/^[ \t]+|[()0-9]+/,"",$2); sub(/^[ \t]+/, "", $2); print $2}')
 
-  if [ "$wifi_status" == "disconnected" ]; then
+  if [ "$wifi_status" == "connected" ]; then
+    echo "$(date): A WiFi device was found and is connected." >> "$LOG_FILE"
+  elif [ "$wifi_status" == "disconnected" ]; then
     echo "$(date): A WiFi device was found, but it is disconnected. Running usb_reset..." >> "$LOG_FILE"
     "$USB_RESET_SCRIPT" "$USB_PATH"
-  elif [ "$wifi_status" == "connected" ]; then
-    echo "$(date): A WiFi device was found and is connected." >> "$LOG_FILE"
   else
     echo "$(date): No WiFi device found on $USB_PATH" >> "$LOG_FILE"
   fi
 
-  sleep 120 # Wait for 2 minutes
+  sleep 10 # Wait for 2 minutes
 done
