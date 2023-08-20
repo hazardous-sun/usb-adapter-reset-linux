c58f0fc7fd299954c56b717e0e4ad43a0331f947
solaire-Pc <contact@unharmfulhazzard.com>
2023-08-20 13:46:13 -0300

    Added a check to see if the device connected to the specified USB port is just disconnected from Wi-Fi or if there is no USB adapter connected at all

--- wifi_check.sh
+++ wifi_check.sh
@@ -9,10 +9,14 @@ LOG_FILE="$SCRIPT_DIR/reset_script.log"
 while true; do
   # Run nmcli command and check for "disconnected" keyword
   if nmcli device | grep -q "disconnected"; then
+      echo "$(date): A connected device was found, but it is disconnected. Running usb_reset..." >> "$LOG_FILE"
+            "$USB_RESET_SCRIPT" "$USB_PATH"
+  # Run nmcli command and check for "unavailable" keyword
+  elif nmcli device | grep -q "unavailable"; then
       echo "$(date): No connected device found. Running usb_reset..." >> "$LOG_FILE"
       "$USB_RESET_SCRIPT" "$USB_PATH"
   else
       echo "$(date): A connected device was found." >> "$LOG_FILE"
   fi
-  sleep 120 # Wait for 120 seconds
+  sleep 120 # Wait for 2 minutes
 done
